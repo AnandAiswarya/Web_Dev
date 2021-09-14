f4b91a2da10269d9d65393adfdbe4763eb85eec4
Aiswarya Anand <anandaiswarya2109@gmail.com>
2021-09-14 21:59:41 +0530

    added new

---
+++ addubday/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+
+import React from "react";
+import About from "./components/About";
+
+import Navbar from "./components/Navbar";
+
+import Skills from "./components/Skills";
+import "tailwindcss/tailwind.css"
+
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Skills />
+    </main>
+  );
+}
---
+++ addubday/src/data.js
@@ -0,0 +1,3 @@
+export const skills = [
+    "School pupil leader" , "Olympiad Medalist" ,"Cricketer" , "Debater" , "Quizzer" , "Arts" , "Public Speaking" , "Event Manager" , "Problem Solver"
+];
